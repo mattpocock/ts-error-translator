c40847d1707f1b230749e9725517ba7ce88c6315
YassinEldeeb <yassineldeeb94@gmail.com>
2022-04-28 17:12:54 +0200

    fix(scripts): exiting the process with error code in the proper places

--- scripts/translate.ts
+++ scripts/translate.ts
@@ -6,23 +6,24 @@ const errorCode = process.argv[2]
 
 if (!errorCode) {
   logger.error(`\nYou've to provide an error code for your translation!`)
-  process.exit(1);
   logger.info(`Example:`)
   logger.info(`  yarn translate 1006`)
-} else {
-  const template = fs.readFileSync(
-    path.join(__dirname, './templates/error.md'),
-    'utf-8'
-  )
-
-  const errorPath = `packages/engine/errors/${errorCode}.md`
-
-  if (fs.existsSync(errorPath)) {
-    logger.error(`\nTranslation for error code ${errorCode} already exists!`)
-  } else {
-    fs.writeFileSync(errorPath, template)
-
-    logger.success('\nTemplate has been written Successfuly!')
-    logger.info(`Check: ${errorPath}`)
-  }
+  process.exit(1)
 }
+
+const template = fs.readFileSync(
+  path.join(__dirname, './templates/error.md'),
+  'utf-8'
+)
+
+const errorPath = `packages/engine/errors/${errorCode}.md`
+
+if (fs.existsSync(errorPath)) {
+  logger.error(`\nTranslation for error code ${errorCode} already exists!`)
+  process.exit(1)
+}
+
+fs.writeFileSync(errorPath, template)
+
+logger.success('\nTemplate has been written Successfuly!')
+logger.info(`Check: ${errorPath}`)
