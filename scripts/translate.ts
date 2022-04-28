5fd30c5348cca9909f885dadc54fbab53132eb0d
YassinEldeeb <yassineldeeb94@gmail.com>
2022-04-28 17:30:00 +0200

    Revert "fix(scripts): exiting the process with error code in the proper places"
    This reverts commit 91e550a682542e7f23e7253770a9c72ffef1d54e.

--- scripts/translate.ts
+++ scripts/translate.ts
@@ -6,24 +6,23 @@ const errorCode = process.argv[2]
 
 if (!errorCode) {
   logger.error(`\nYou've to provide an error code for your translation!`)
+  process.exit(1);
   logger.info(`Example:`)
   logger.info(`  yarn translate 1006`)
-  process.exit(1)
+} else {
+  const template = fs.readFileSync(
+    path.join(__dirname, './templates/error.md'),
+    'utf-8'
+  )
+
+  const errorPath = `packages/engine/errors/${errorCode}.md`
+
+  if (fs.existsSync(errorPath)) {
+    logger.error(`\nTranslation for error code ${errorCode} already exists!`)
+  } else {
+    fs.writeFileSync(errorPath, template)
+
+    logger.success('\nTemplate has been written Successfuly!')
+    logger.info(`Check: ${errorPath}`)
+  }
 }
-
-const template = fs.readFileSync(
-  path.join(__dirname, './templates/error.md'),
-  'utf-8'
-)
-
-const errorPath = `packages/engine/errors/${errorCode}.md`
-
-if (fs.existsSync(errorPath)) {
-  logger.error(`\nTranslation for error code ${errorCode} already exists!`)
-  process.exit(1)
-}
-
-fs.writeFileSync(errorPath, template)
-
-logger.success('\nTemplate has been written Successfuly!')
-logger.info(`Check: ${errorPath}`)
