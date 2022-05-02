8a434cd9ccd719f88ce714746d84e867edbb7dea
Matt Pocock <mattpocockvoice@gmail.com>
2022-05-02 16:20:28 +0100

    Improved script and added 2339

--- scripts/translate.ts
+++ scripts/translate.ts
@@ -1,26 +1,36 @@
 import fs from 'fs';
 import path from 'path';
 import { logger } from './utils/logger';
+import tsErrorMessages from '../packages/engine/src/tsErrorMessages.json';
 
 const errorCode = process.argv[2];
 
+const getTemplate = (original: string) =>
+  `---
+original: "${original}"
+excerpt: "Simplified version of the error message"
+---
+
+More details, reproducible examples for the error and how the error should be fixed.
+
+`.trim();
+
 if (!errorCode) {
-  logger.error(`\nYou've to provide an error code for your translation!`);
-  process.exit(1);
+  logger.error(`\nYou haven't provided an error code for your translation!`);
   logger.info(`Example:`);
   logger.info(`  yarn translate 1006`);
+  process.exit(1);
 } else {
-  const template = fs.readFileSync(
-    path.join(__dirname, './templates/error.md'),
-    'utf-8',
-  );
-
   const errorPath = `packages/engine/errors/${errorCode}.md`;
 
   if (fs.existsSync(errorPath)) {
     logger.error(`\nTranslation for error code ${errorCode} already exists!`);
   } else {
-    fs.writeFileSync(errorPath, template);
+    const originalError = Object.keys(tsErrorMessages).find((key) => {
+      return String(tsErrorMessages[key].code) === errorCode;
+    });
+
+    fs.writeFileSync(errorPath, getTemplate(originalError));
 
     logger.success('\nTemplate has been written Successfuly!');
     logger.info(`Check: ${errorPath}`);
