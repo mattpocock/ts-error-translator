8c15822da9b80ccee723bf796610131c71d583bb
Tom Jenkinson <tom@tjenkinson.me>
2022-09-16 10:07:57 +0100

    refactor props and disable submit button when no text

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -7,23 +7,34 @@ import {
   compressToEncodedURIComponent,
   decompressFromEncodedURIComponent,
 } from 'lz-string';
-import { GetServerSidePropsContext } from 'next';
+import { GetServerSideProps, GetServerSidePropsContext } from 'next';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import * as path from 'path';
-import React from 'react';
+import React, { useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import { z } from 'zod';
 
-export default function Web(props: { error: string; errors: ErrorInfo[] }) {
+type Result = { type: 'none' } | {
+  type: 'errors',
+  errors: ErrorInfo[]
+}
+
+type Props = {
+  result: Result,
+  error: string,
+}
+
+export default function Web(props: Props) {
   const router = useRouter();
+  const [inputContainsText, setInputContainsText] = useState(!!props.error);
 
-  const firstExcerpt = props.errors[0]?.improvedError?.excerpt;
-  const firstErrorCode = props.errors[0]?.code;
+  const firstError = props.result.type === 'errors' ? props.result.errors[0] ?? null : null;
+  const firstExcerpt = firstError?.improvedError?.excerpt ?? null;
+  const firstErrorCode = firstError?.code ?? null;
 
-  const title = `TypeScript Error Translator${
-    firstErrorCode ? ` | Code #${firstErrorCode}` : ''
-  }`;
+  const title = `TypeScript Error Translator${firstErrorCode ? ` | Code #${firstErrorCode}` : ''
+    }`;
 
   return (
     <>
@@ -96,63 +107,67 @@ export default function Web(props: { error: string; errors: ErrorInfo[] }) {
               name="error"
               autoFocus
               defaultValue={props.error}
+              onChange={(e) => setInputContainsText(!!e.target.value)
+              }
             ></textarea>
             <div>
-              <button className="px-6 py-2 font-semibold tracking-tight text-white rounded from-purple-500 to-indigo-600 bg-gradient-to-r focus:outline-none focus:ring-4 ring-yellow-400">
+              <button disabled={!inputContainsText} className="px-6 py-2 font-semibold tracking-tight text-white rounded from-purple-500 to-indigo-600 bg-gradient-to-r focus:outline-none focus:ring-4 ring-yellow-400 disabled:bg-slate-300 disabled:bg-none disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none">
                 Submit your Error
               </button>
             </div>
           </form>
         </div>
 
-        <div className="max-w-2xl mx-auto space-y-16 text-xl leading-relaxed text-gray-800">
-          {props.errors.map((error, index, array) => {
-            return (
-              <div key={index}>
-                <div className="prose prose-code:before:hidden prose-code:after:hidden">
-                  {/* <span className="inline-block px-2 py-1 mb-2 text-xs text-indigo-900 bg-indigo-100 rounded">
-                  #{error.code}
-                </span> */}
-                  <h1>Error #{array.length - index}</h1>
-                  <div className="relative p-4 py-3 font-mono text-sm leading-relaxed text-gray-100 bg-gray-800 rounded">
-                    {error.parseInfo.rawError}
+        {props.result.type === 'errors' && (
+          <div className="max-w-2xl mx-auto space-y-16 text-xl leading-relaxed text-gray-800">
+            {props.result.errors.map((error, index, array) => {
+              return (
+                <div key={index}>
+                  <div className="prose prose-code:before:hidden prose-code:after:hidden">
+                    {/* <span className="inline-block px-2 py-1 mb-2 text-xs text-indigo-900 bg-indigo-100 rounded">
+                    #{error.code}
+                  </span> */}
+                    <h1>Error #{array.length - index}</h1>
+                    <div className="relative p-4 py-3 font-mono text-sm leading-relaxed text-gray-100 bg-gray-800 rounded">
+                      {error.parseInfo.rawError}
+                    </div>
+                    {error.improvedError && (
+                      <>
+                        <h2>Translation</h2>
+                        <div className="p-4 py-3 font-light text-gray-100 bg-gray-800 rounded prose-code:text-white prose-p:m-0">
+                          <ReactMarkdown>
+                            {error.improvedError.excerpt}
+                          </ReactMarkdown>
+                        </div>
+                        <h2>Explanation</h2>
+                        <ReactMarkdown>{error.improvedError?.body}</ReactMarkdown>
+                      </>
+                    )}
+                    {!error.improvedError && (
+                      <>
+                        <h2>Translation</h2>
+                        <p>
+                          Could not find a translation for error code{' '}
+                          <span className="font-semibold">#{error.code}</span>:
+                        </p>
+                        <code>{error.error}</code>
+                        <p>
+                          <a
+                            href="https://github.com/mattpocock/ts-error-translator/blob/main/CONTRIBUTING.md"
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            Add a translation
+                          </a>
+                        </p>
+                      </>
+                    )}
                   </div>
-                  {error.improvedError && (
-                    <>
-                      <h2>Translation</h2>
-                      <div className="p-4 py-3 font-light text-gray-100 bg-gray-800 rounded prose-code:text-white prose-p:m-0">
-                        <ReactMarkdown>
-                          {error.improvedError.excerpt}
-                        </ReactMarkdown>
-                      </div>
-                      <h2>Explanation</h2>
-                      <ReactMarkdown>{error.improvedError?.body}</ReactMarkdown>
-                    </>
-                  )}
-                  {!error.improvedError && (
-                    <>
-                      <h2>Translation</h2>
-                      <p>
-                        Could not find a translation for error code{' '}
-                        <span className="font-semibold">#{error.code}</span>:
-                      </p>
-                      <code>{error.error}</code>
-                      <p>
-                        <a
-                          href="https://github.com/mattpocock/ts-error-translator/blob/main/CONTRIBUTING.md"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          Add a translation
-                        </a>
-                      </p>
-                    </>
-                  )}
                 </div>
-              </div>
-            );
-          })}
-        </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     </>
   );
@@ -162,31 +177,33 @@ const Query = z.object({
   error: z.string().optional(),
 });
 
-export const getServerSideProps = (ctx: GetServerSidePropsContext) => {
+export const getServerSideProps: GetServerSideProps<Props> = async (ctx: GetServerSidePropsContext) => {
   const query = Query.parse(ctx.query);
   if (query.error) {
     const decodedError = decompressFromEncodedURIComponent(query.error)!;
     return {
       props: {
-        errors: parseErrors(decodedError)
-          .reverse()
-          .map((error): ErrorInfo => {
-            return {
-              ...error,
-              improvedError: getImprovedMessageFromMarkdown(
-                path.resolve(process.cwd(), '../../packages/engine/errors'),
-                error.code,
-                error.parseInfo.items,
-              ),
-            };
-          }),
+        result: {
+          type: 'errors', errors: parseErrors(decodedError)
+            .reverse()
+            .map((error): ErrorInfo => {
+              return {
+                ...error,
+                improvedError: getImprovedMessageFromMarkdown(
+                  path.resolve(process.cwd(), '../../packages/engine/errors'),
+                  error.code,
+                  error.parseInfo.items,
+                ),
+              };
+            })
+        },
         error: decodedError,
       },
     };
   }
   return {
     props: {
-      errors: [],
+      result: { type: 'none' },
       error: `Conversion of type 'string' to type 'string[]' may be a mistake because neither type sufficiently overlaps with the other. If this was intentional, convert the expression to 'unknown' first.`,
     },
   };
